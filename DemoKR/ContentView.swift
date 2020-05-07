51e3bcafe109988dc78f7662fdf9d1893adc30d2
Paul <pasha.st@gmail.com>
2020-05-07 13:36:58 +1000

    Initial Commit

---
+++ DemoKR/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoKR
+//
+//  Created by Павел Тимош on 07.05.2020.
+//  Copyright © 2020 Павел Тимош. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
